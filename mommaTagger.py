7a625bb0d05966586711d976b9c701c4bcb72eb8
AdamKing11 <adamking@email.arizona.edu>
2016-11-11 16:42:19 -0700

    moved saving/loading, took out Bayesian classifier for tagging, just use trigram probs

--- mommaTagger.py
+++ mommaTagger.py
@@ -6,25 +6,25 @@ from word_features import *
 
 class mommaTagger:
 
-
-	mommaTagger = 0
 	accuray = 1.
 
 	tag_trigrams = {} # we hold in counts for each tag trigram
 
 	def __init__(self, tagged_c, verbose = True):
 		"""
-
+		trains a trigram probability classifier
+		we load in the tagged corpus from the babyTagger file
+		and use that to calculate trigram probabilites for tags
 		"""
-		self.read_tag_trigrams(tagged_c)
-		self.train_momma_classifier()
-
+		self.tag_trigrams = self.read_tag_trigrams(tagged_c)
+		
 	def read_tag_trigrams(self, baby_tagged_file, threshold = 1., verbose = True):
 		"""
 		read in a file tagged by the baby tagger and get all tag
 		trigrams where ALL tokens in the trigram beat our threshold
 		(default 1, so we only do unambiguous tokens)
 		"""
+		trigrams = {}
 		i, j = 1, 0
 		with open(baby_tagged_file, "r") as rF:
 			# go through lines
@@ -58,93 +58,69 @@ class mommaTagger:
 				if len(tag_history) == 3:
 					j += 1
 					tag_trigram = tuple([t[0] for t in tag_history])
-					if tag_trigram in self.tag_trigrams:
-						self.tag_trigrams[tag_trigram] += 1
+					if tag_trigram in trigrams:
+						trigrams[tag_trigram] += 1
 					else:
-						self.tag_trigrams[tag_trigram] = 1
+						trigrams[tag_trigram] = 1
 					# now, we pop the OLDEST guy off the list
 					tag_history.pop(0)
 		if verbose:
 			print("\nRead in", i-1, "sentences and found", j, "trigrams with", \
 				len(self.tag_trigrams), "trigram types.")
+		return trigrams
 
-	def train_momma_classifier(self, verbose = True):
+	def quick_tag(self, possible_tags, previous, following, trigram_weight=3):
 		"""
-		takes a bunch of tag trigrams and builds a Naive Bayesian train_momma_classifier
-		that will judge a particular POS based on the preceding and following tag
+		given a tag of the previous word and the tag of the following word, based
+		on what we've seen, we return the most likely tag for target word
+
+		MAYBE implement EM in future versions?
 		"""
+		tag_probabilities = {}
+		for p in possible_tags:
+			# probability of current tag given previous
+			# which is the SUM of all trigrams where the FIRST TWO are the same,
+			# divided by all trigrams where the FIRST one is the same
+			forward = sum([self.tag_trigrams[c] for c in self.tag_trigrams \
+				if c[0] == previous and c[1] == p]) / sum([self.tag_trigrams[c] \
+				for c in self.tag_trigrams if c[0] == previous])
+			# probability of current tag given the following tag
+			# SUM of all trigrams where LAST TWO are same, divided by sum where
+			# LAST one is same
+			backward = sum([self.tag_trigrams[c] for c in self.tag_trigrams \
+				if c[2] == following and c[1] == p]) / sum([self.tag_trigrams[c] \
+				for c in self.tag_trigrams if c[2] == following])
 
-		training_set = []
-		if verbose:
-			print("\nNow training Naive Bayesian Classifier on tag trigrams.")
-		for t in self.tag_trigrams:
-			features = {}			# create new dict for the features
-			
-			# we find the ratio of this trigram to all other trigrams with
-			# the same context. This way, we weight more frequent trigrams
-			# higher #Bayes #prior
-			
-			# bin the ratios into 20%, 40%, 60%... etc
-			features['previous'] = round(5 * (self.tag_trigrams[t] \
-				/ sum([self.tag_trigrams[c] for c in self.tag_trigrams \
-				if c[0]==t[0]])))
+			whole_context = self.tag_trigrams[(previous,p,following)] / \
+				sum([self.tag_trigrams[c] for c in 	self.tag_trigrams if \
+				c[0] == previous and c[2] == following])
 
-			features['following'] = round(5 * (self.tag_trigrams[t] \
-				/ sum([self.tag_trigrams[c] for c in self.tag_trigrams \
-				if c[2]==t[2]])))
+			# now we take all 3 calculated probabilites and normalize them
+			# we also weight the trigram probability HIGHER than the bigrams
+			combined_prob = (forward + backward + \
+				(whole_context * trigram_weight)) / (2 + trigram_weight)
+			tag_probabilities[p] = combined_prob
 
-			features['whole-context'] = round(5 * (self.tag_trigrams[t] \
-				/ sum([self.tag_trigrams[c] for c in self.tag_trigrams \
-				if c[0]==t[0] and c[2]==t[2]])))
-
-			
-			training_set.append((features, t[1]))	# we add this to the training set...
-
-		if verbose:
-			print("Training with", len(training_set), "data.")
-		# randomize the order
-		shuffle(training_set)
-
-		self.mommaTagger = nltk.NaiveBayesClassifier.train(training_set)
-
-	def test_momma_classifier(self, k, verbose = True):
-		"""
-		does a quick k-fold test on the classifier we've built... or at least on
-		the features/data we've used to make the classifier....
-		"""
-		training_set = []
-		
-		for t in self.tag_trigrams:
-			features = {}			# create new dict for the features
-			
-			features['previous'] = round(5 * (self.tag_trigrams[t] \
-				/ sum([self.tag_trigrams[c] for c in self.tag_trigrams \
-				if c[0]==t[0]])))
-
-			features['following'] = round(5 * (self.tag_trigrams[t] \
-				/ sum([self.tag_trigrams[c] for c in self.tag_trigrams \
-				if c[2]==t[2]])))
-
-			features['whole-context'] = round(5 * (self.tag_trigrams[t] \
-				/ sum([self.tag_trigrams[c] for c in self.tag_trigrams \
-				if c[0]==t[0] and c[2]==t[2]])))
-
-			training_set.append((features, t[1]))	# we add this to the training set...
-
-
-		# shuffle the order of the list
-		shuffle(training_set)
-		self.accuracy, _ = cross_validation(training_set, k, verbose = verbose)
+		return tag_probabilities
 
 
 
 if __name__ == "__main__":
-	momma = mommaTagger("tagged.txt")
-	momma.test_momma_classifier(6)
-	for t in sorted(momma.tag_trigrams, reverse=True):
-		l = sum([momma.tag_trigrams[x] for x in momma.tag_trigrams if x[0]==t[0] and x[2]==t[2]])
-		#print(t, momma.tag_trigrams[t], l)
-	momma.mommaTagger.show_most_informative_features()
-		
-	#print(len(momma.tag_trigrams))
-
+	#momma = mommaTagger("tagged.txt")
+	#momma.test_momma_classifier(6)
+	#momma.mommaTagger.show_most_informative_features()
+	
+	#c_save(momma, "m_tagger.t")
+	momma = c_load("m_tagger.t")
+	print("V\t\tN")
+	print(momma.tag_trigrams[("A", "V", "A")], momma.tag_trigrams[("A", "N", "A")])
+	print(momma.quick_tag(("N","V"),"A","A"))
+	
+	print(momma.tag_trigrams[("N", "V", "A")], momma.tag_trigrams[("N", "N", "A")])
+	print(momma.quick_tag(("N","V"),"N","A"))
+	
+	print(momma.tag_trigrams[("N", "V", "N")], momma.tag_trigrams[("N", "N", "N")])
+	print(momma.quick_tag(("N","V"),"N","N"))
+	
+	print(momma.tag_trigrams[("V", "V", "V")], momma.tag_trigrams[("V", "N", "V")])
+	print(momma.quick_tag(("N","V"),"V","V"))
