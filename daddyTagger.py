5e6c47ecb0ee68aaebf0dec902fca3b8ffb15025
AdamKing11 <adamking@email.arizona.edu>
2016-11-13 12:11:28 -0700

    major overhaul, finished (inshallah) the gap finder function

--- daddyTagger.py
+++ daddyTagger.py
@@ -71,49 +71,72 @@ class daddyTagger:
 
 	def gap_fill(self, s, threshold = .9):
 		"""
-		we're going to find the best fit of tags for a given substring (by considering all)
-		possible
+		all right! going to use a quasi-Forward-Backward algorithm to find the best sequence
+		of tags GIVEN a definite, known tag at the beginning and at the end
+		That is, we know T_1 and T_4, so we need to find the best T_2 and T_3 to make a good
+		sequence
 
-		because we do this in small chunks, we reduce that HUGE factorial amount of possiblities
-		we'd need to consider for the whole string....
-
-		okay, this is going to be fun. Because we have a length s-2 string, with up to 3 possible
-		tags for each word AND up to 9 possible tag trigrams, we are going  to be building a 3D
-		matrix of size (s-2) * 3 * 9....
-		and doing 3d viterbi
+		We're going to use the trigram probabilities of tag sequences from our unambiguously 
+		tagged sentences
 		""" 
+	
 		# get the first and last tag of the sentence part
 		first_tag = self.babyTagger.quick_tag(s[0])[2][1]
 		last_tag = self.babyTagger.quick_tag(s[-1])[2][1]
 		
-		# need to calculate exactly how many possible sentences we'll be considering...
-		
+		word_tag_dict = {}	# probability of a TAG given a word
+		# ie word_tag_dict[("dog", "N")] = .4 if there's a .4 chance dog is a noun
+
+		# we put the first and last tags as identical lists of 3, so the matrix will
+		# always be of stable dimensions
+		prob_matrix = numpy.zeros((len(s)-2,3))
 		tag_matrix = [[first_tag]]
 		# first, make matrix of all possible tags....
 		for i in range(len(s)-2):
 			w = s[i+1]
-			pos_tags = self.babyTagger.quick_tag(w)[2]
-			tags = [pos_tags[k] for k in np.arange(1,len(pos_tags),2)]
-			tag_matrix.append(tags)
-		tag_matrix.append([last_tag])
-		
-		prob_matrix = np.ones((len(s)-2,3,3))
-		# loop through all unknown tags (we know the edges are good so len-2)
-		for i in range(len(s)-2):
-			w = s[i+1]
+			
 			pos_tags = self.babyTagger.quick_tag(w)[2]
 			probs = [pos_tags[k] for k in np.arange(0,len(pos_tags),2)]
 			tags = [pos_tags[k] for k in np.arange(1,len(pos_tags),2)]
-			# from 0 to end of pos_tags, step by 2 (because we want to skip the actual tags)
-			for j in range(len(probs)):
-				for k in range(len(tag_matrix[j])):
-					print(tag_matrix[j][k])
-				prob_matrix[i,j,0] *= probs[j]
-				
-			
-		print(prob_matrix)
+			tag_matrix.append(tags)
+			for j in range(3):
+				word_tag_dict[(w,tags[j])] = probs[j]
+
+	
+		# for the first unknown, we use the MIDDLE trigram probs given the unambiguous first tag,
+		# and the sum over all possible following tags
+		for i in range(3):
+			# just so I know what the variables are....
+			word = s[1]
+			this_tag = tag_matrix[1][i]
+			# EMission... chance that a given word is that tag
+			em = word_tag_dict[(w, this_tag)]
+			# TRansition... sum of all chances that this is the tag, given possible starts and 3rd states
+			tr = sum([self.mommaTagger.prob_middle((first_tag, next_tag), this_tag) for next_tag in tag_matrix[2]]) 
+			prob_matrix[0,i] = em * tr
+
+		# for subsequent tags, we use the sum over the probabilites of the previous TWO tags	
+		for i in range(1,len(s)-2):
+			word = s[i]
+			pos_prev = [(laster, last) for laster in tag_matrix[i-1] for last in tag_matrix[i-2]]
+			for j in range(3):
+				this_tag = tag_matrix[i][j]
+				em = word_tag_dict[(w, this_tag)]
+				tr = self.mommaTagger.prob_last(pos_prev[j], this_tag)
+				prob_matrix[i,j] = tr * em * sum(prob_matrix.sum(i-1)) 
+
+		# okay! now we have a matrix of all possible tag sequences. We just go through the matrix and pick
+		# the best from each column and find the corresponding tag
+		tag_matrix = tag_matrix[1:]
+		gap_guess = []
+		for i in range(len(s)-2):
+			guess_index = np.argmax(prob_matrix[i])
+			gap_guess.append((tag_matrix[i][guess_index], prob_matrix[i,guess_index]))
+		print(gap_guess)
 
 
+		
+		
 
 
 if __name__ == "__main__":
