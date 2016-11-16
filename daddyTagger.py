f2a095240902df5244ef0c50169bba9f4afec187
AdamKing11 <adamking@email.arizona.edu>
2016-11-15 17:18:34 -0700

    still tweaking contextual prob... also moved large code block to make tag_corpus function part of class

--- daddyTagger.py
+++ daddyTagger.py
@@ -39,7 +39,24 @@ class daddyTagger:
 		self.babyTagger = bt
 		self.mommaTagger = mt
 
-	def tag(self, s, threshold = .9):
+	def say_hello(self):
+		"""
+		display information about the taggers
+		"""
+		mtc = sum(self.mommaTagger.tag_trigrams[t] for t in self.mommaTagger.tag_trigrams)
+
+		print("Baby Tagger:")
+		print("\tAccuracy:", self.babyTagger.accuracy)
+		print("\tUnambigous Tokens:", len(self.babyTagger.Unambig))
+		print("\tAmbigous Tokens:", len(self.babyTagger.ambig))
+		#print("\tTag types:", len(self.babyTagger.all_tags))
+
+		print("Momma Tagger:")
+		print("\tTag types:", len(self.mommaTagger.all_tags))
+		print("\tTrigram types:", len(self.mommaTagger.mid_prob))
+		print("\tTotal trigrams:", mtc)
+
+	def tag(self, s, threshold = .9, fc = True):
 		"""
 			Tag a sentence! (duh)
 			We first go through and find all unambiguous words in the sentence
@@ -56,7 +73,11 @@ class daddyTagger:
 		# first pass, find and tag all unambiguous tokens or reeeeallly high prob
 		# unknown words
 		for i in range(len(s)):
-			w = s[i]
+			# fold case?
+			if fc:
+				w = s[i].lower()
+			else:
+				w = s[i]
 			g = self.babyTagger.quick_tag(w)
 				# if it's unambiguous....
 			if g[0] == 1:
@@ -117,14 +138,17 @@ class daddyTagger:
 		first_tag = self.babyTagger.quick_tag(s[0])[2][1]
 		final_tag = self.babyTagger.quick_tag(s[-1])[2][1]
 		
+		mc = 4		# for maximum candidates, ie how many possible pos labels we allow per token
+
 		# dictionary for storing the probability a given word has a particular tag
 		# because we're going to be copying some tags for a given word, this will be 1/3
 		# for ambiguous and unambiguous tokens to indicate that any tag is possible
 		word_tag_dict = {}
 		
-		prob_matrix = numpy.zeros((len(s),3))
+		prob_matrix = numpy.zeros((len(s),mc))
 		tag_matrix = []
 
+		
 		for i in range(len(s)):
 			w = s[i]
 			
@@ -132,21 +156,30 @@ class daddyTagger:
 			probs = [pos_tags[k] for k in np.arange(0,len(pos_tags),2)]
 			tags = [pos_tags[k] for k in np.arange(1,len(pos_tags),2)]
 
-			print("\t", w, tags)
+#			print("\t", w, tags)
 			if len(tags) == 1:	
 				# if it's an unambigous word, ie the first and last
 				# we just copy the SINGLE tag 3 times
-				tag_matrix.append([tags[0] for _ in range(3)])
-				word_tag_dict[(w, tags[0])] = 1./3
+				tag_matrix.append([tags[0] for _ in range(mc)])
+				word_tag_dict[(w, tags[0])] = 1./mc
 			elif len(tags) == 2:
 				# if it's an ambiguous token with 2 tags, just copy the last one
-				tag_matrix.append([tags[0], tags[1], tags[1]])
-				word_tag_dict[(w, tags[0])] = 1./3
-				word_tag_dict[(w, tags[1])] = 1./3
+				tag_list = [tags[0]]
+				word_tag_dict[(w, tags[0])] = 1./mc
+				for j in range(1,mc):
+					word_tag_dict[(w, tags[1])] = 1./mc
+					tag_list.append(tags[1])
+				tag_matrix.append(tag_list)
 			else:
-				tag_matrix.append(tags)
-				for j in range(3):
+				tag_list = tags
+				for j in range(len(tags)):
 					word_tag_dict[(w, tags[j])] = probs[j]
+				# if we have a prob distribution BUT we still have fewer possible 
+				# tags than we'd like, just copy the LAST one
+				if len(tags) < mc:
+					for j in range(len(tags),mc):
+						tag_list.append(tags[len(tags)-1])
+				tag_matrix.append(tag_list)
 
 		for i in range(1,len(s)-1):
 			w = s[i]
@@ -155,7 +188,7 @@ class daddyTagger:
 			pos_next = tag_matrix[i+1]
 			pos_contexts = [(p, n) for p in pos_prev for n in pos_next]
 
-			for j in range(3):
+			for j in range(mc):
 				# we sum over the probabilities for ALL possible contexts (previous and following
 				#	tags given the current POSSIBLE tag)
 				tag = tag_matrix[i][j]
@@ -172,26 +205,117 @@ class daddyTagger:
 				prob_matrix[i,j] = em*tr
 
 			# now we normalize the probs...
-			for j in range(3):
+			for j in range(mc):
 				prob_matrix[i,j] = prob_matrix[i,j] / prob_matrix[i].sum() 
 
 		gap_guess = []
 		for i in range(1,len(s)-1):
 			guess_index = np.argmax(prob_matrix[i])
 			reg_confidence = prob_matrix[i,guess_index] / prob_matrix[i,].sum()
+			if prob_matrix[i,].sum() == 0:
+				print("WHOA!")
 			gap_guess.append((tag_matrix[i][guess_index], reg_confidence))
 		return gap_guess
 
+	def tag_corpus(self, corpus, verbose = True):
+		"""
+		"""
+		running = []
+		goldsX = []
+		goldsY = []
+		golds = 0
+
+
+	# for saving our silver boys...
+		with open("golds/" + corpus + ".silver.X.txt", "w") as wF:
+			pass
+		with open("golds/" + corpus + ".silver.Y.txt", "w") as wF:
+			pass
+		with open("golds/" + corpus + ".silver.M.txt", "w") as wF:
+			pass
 	
 
+#	for t in m.all_tags:
+#		print(t)
+		if verbose:
+			print("\n" + "-" * 10 + corpus + "-" * 10 + "\n")
+	# let's load in some sentences....
+		with open(corpus + ".READY.txt", "r") as rF:
+			i = 0
+			for line in rF:
+				line = line.rstrip()
+				s = line.rsplit(" ")
+			# get rid of any " " values that may be in there, messing up the stuff
+			# damn gremlins
+			# mogwai
+				s = [w for w in s if w != ""]
+
+				if len(s) > 7:
+					i += 1
+					if verbose:
+						print(golds, "of", i, "sentences.", end="\r")
+
+					if i < 0:
+						continue
+					if i > 10000:
+						pass	# do them all!!!!
+						break
+					t, g = daddy.tag(s)
+					conf = 1.
+					if len(g) > 1:
+					# if there are some guesses, let's check to see the mean 
+					# confidence in those guesses
+						conf = sum(s[1] for s in g)/len(g)
+					else:
+						conf = 1.0
+					if not np.isnan(conf):
+						running.append(conf)
+					tagged_s = ""
+
+					if conf >= 1.0:
+					#print(s)
+					#print(conf, len(g), g)
+						for j in range(len(s)):
+							tagged_s += s[j] + "_" + t[j] + " "
+						tagged_s = tagged_s[0:-1]
+
+						g_X = s[0]
+						g_Y = t[0]
+						golds+=1
+						for j in range(1, len(s)):
+							g_X += "\t" + s[j]
+							g_Y += "\t" + t[j]
+						with open("golds/" + corpus + ".silver.X.txt", "a") as wF:
+							wF.write(g_X + "\n")
+						with open("golds/" + corpus + ".silver.Y.txt", "a") as wF:
+							wF.write(g_Y + "\n")
+						with open("golds/" + corpus + ".silver.M.txt", "a") as wF:
+							wF.write(tagged_s + "\n")
+
+		if verbose:		
+			print("Over", len(running), "sentences, we have a mean confidence of", np.mean(running), \
+				"with a variance of", np.var(running))	
+			print(golds, "silver sentences in the bunch.\n")
+
+
 if __name__ == "__main__":
+	print("Loading the 2 taggers...")
 	# load the 2 sub-taggers
 	b = c_load("taggers/b_tagger.t")
 	m = c_load("taggers/m_tagger.t")
 	# make the big one
+	print("Building the final tagger...")
 	daddy = daddyTagger(b, m)
-	c_save("taggers/d_tagger.t")
+	c_save(daddy, "taggers/d_tagger.t")
 
+	daddy.say_hello()
+	
+	corpus = "hyWiki"
+	corpus = "EANC"
+	daddy.tag_corpus(corpus)
+
+
+	"""
 	s = "<s> Դուք կարող եք դիտել կամ պատճենել այս էջի կոդը ։ </s>"
 	#s = "<s> Դուք կարող եք"
 	s = s.rsplit(" ")
@@ -200,60 +324,4 @@ if __name__ == "__main__":
 		print(s[i] + "_" + z[i], end = " ")
 	print()
 	print(g)
-	
-"""
-
-	running = []
-	goldsX = []
-	goldsY = []
-
-	# for saving our golden boys...
-	with open("golds/golds.X.txt", "w") as wF:
-		pass
-	with open("golds/golds.Y.txt", "w") as wF:
-		pass
-	
-	# let's load in some sentences....
-	with open("hyWiki_sub.txt", "r") as rF:
-		i = 0
-		for line in rF:
-			line = line.rstrip()
-			s = line.rsplit(" ")
-			if len(s) > 7:
-				i += 1
-				if i > 10000:
-					pass	# do them all!!!!
-					#break
-				t, g = daddy.tag(s)
-				if len(g) > 1:
-					conf = sum(s[1] for s in g)/len(g)
-				else:
-					conf = 1
-				running.append(conf)
-				tagged_s = ""
-
-				for j in range(len(s)):
-					tagged_s += s[j] + "_" + t[j] + " "
-					pass
-				if conf == 1:
-					g_X = s[0]
-					g_Y = t[0]
-					for j in range(1, len(s)):
-						g_X += "\t" + s[j]
-						g_Y += "\t" + t[j]
-					with open("golds.X.txt", "a") as wF:
-						wF.write(g_X + "\n")
-					with open("golds.Y.txt", "a") as wF:
-						wF.write(g_Y + "\n")
-
-
-				tagged_s = tagged_s[0:-1]
-				print(i, end="\r")
-				#print(tagged_s)
-				#print(len(g), conf, "\n")
-				
-	print("Over", len(running), "sentences, we have a mean confidence of", np.mean(running), \
-		"with a variance of", np.var(running))
-				
-	print(len(golds), "gold sentences in the bunch:\n")
 	"""
