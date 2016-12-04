ef7ac63ca5835d23d9c2ceedb329d2d6e822237b
AdamKing11 <adamking@email.arizona.edu>
2016-12-04 13:37:57 -0700

    updated functions, bug fix

--- lib/general.py
+++ lib/general.py
@@ -1,4 +1,5 @@
 import re, sys, pickle
+import nltk, numpy
 
 from lib.word_features import *
 
@@ -165,3 +166,38 @@ def split_tagged_lemma(tagged_lemma):
     lemma = re.sub("\_.*$","",tagged_lemma) 
     tag = re.sub("^.*\_","", tagged_lemma)
     return lemma, tag 
+
+def make_kfold(data, k, total_fold):
+    """
+    returns two lists, the k-th fold of k total folds and the rest of the data
+    """
+    fold_length = int(len(data)/total_fold)
+    fold_start = k * fold_length
+    fold_end = (k + 1) * fold_length
+    # returns TEST, TRAIN
+    return data[fold_start:fold_end], data[:fold_start] + data[fold_end:] 
+
+def cross_validation(data, folds, verbose = True):
+    """
+    Run a k-fold cross validation on the 'data'
+    We split the data up into 'folds' chunks, train on all but 1 chunk and then
+    test on the remaining chunk
+
+    we return the average and the variance for the accuracy of the runs
+    """
+    runs = []
+    if verbose:
+        print("Doing a", str(folds) + "-fold cross validation on", len(data), \
+            "data.")
+    for k in range(folds):    
+    #split into test and train sets
+        test, train = make_kfold(data,k,folds)        
+        
+        b_class = nltk.NaiveBayesClassifier.train(train)
+        runs.append(nltk.classify.accuracy(b_class, test))
+        if verbose:
+            print("\tFold", k+1, " :: ", runs[-1])
+    if verbose:
+        print("Testing complete:\n\tMean:", numpy.mean(runs), "\tVariance:", \
+            numpy.var(runs))
+    return numpy.mean(runs), numpy.var(runs)
