139435cfa6c0aa11f079afb1e35d81d0f91d7a6a
AdamKing11 <adamking@email.arizona.edu>
2016-12-12 16:14:23 -0700

    added an automatic tagging feature for numerals

--- lib/taggers.py
+++ lib/taggers.py
@@ -59,7 +59,7 @@ class babyTagger:
 
 	def build_baby_classifier(self, verbose = True):
 		"""
-    We load in the EANC token file and build a classifier for UNAMBIGUOUS tokens
+    We load in the EANC token file and build a naive Bayesian classifier for UNAMBIGUOUS tokens
     	"""
     	# we take our unambiguous tokens and get them formatted for training
     	# we're also going to save the tag types and counts, just for posterity
@@ -157,6 +157,84 @@ class babyTagger:
 			print("\tAmbiguous:", len(self.ambig))
 
 		self.build_baby_classifier(verbose = verbose)
+
+	def semisupervised_training(self, word_list, iterations = 3, threshold = .99, verbose = True, \
+		folds = 6):
+		"""
+	goes through the 'word_list' and tests all words with the 'baby' babyTagger
+	for all words that beat the 'threshold', we add it to the list of **unambiguous** 
+	tokens in the babyTagger and retrain
+		"""
+		if verbose:
+			print("Going to look through", len(word_list), "to find words that", end = " ")
+			print("score higher than", threshold, "and add them to the classifier.")
+	
+	# if we haven't already tested the classfier, do it now
+	# so we know the overall accuracy at the beginning
+		if self.accuracy == 1:
+			self.test_baby_classifier(folds, verbose = verbose)
+
+	# we're going to get the accuracy of the original classifier
+	# so we know how to 
+		original_accuracy = self.accuracy
+	# loop through the iterations
+		for i in range(iterations):
+			high_scores = {}
+			if verbose:
+				print("\nIteration", i+1)
+				print("\t", len(word_list), "words to check.")
+				j = 0
+			for w in word_list:
+				if verbose:
+					j+=1
+					print("Checking word ::", j, end="\r")
+			# tag the word using the tagger 
+				tag_guess = self.quick_tag(w)
+			# if it's not in the lexicon already AND it's score beats our threshold
+			# add it to the list
+	
+				if tag_guess[0] == 3 and tag_guess[2][0] >= threshold:
+					word = tag_guess[1]
+					tag = tag_guess[2][1]
+					high_scores[word] = word + "_" + tag
+				
+		# if we found no words to add, just exit
+			if len(high_scores) == 0:
+				if verbose:
+					print()
+					print("No new words found to add. Ending training.")
+				break
+		
+		# now that we have the high-scoring words, we delete them from the orginal list
+		# (to avoide adding them again and again) AND we use a random number to determine
+		# if we really want to add them, based on the accuracy of the tagger
+		# that is, if accuracy of the tagger is 90%, we add 90% of the words
+		# if it's 50%, we only add 50%, etc
+			if verbose:
+				print()
+				print("Found", len(high_scores), "words to add.")
+				j = 0
+
+		# go through all the high scoring words and generate a randome number 0-1
+		# if the number is LESS than the accuracy, keep
+		# this way, we don't add too many words to an already bad classifer
+			high_score_to_delete = {}
+			for w in high_scores:
+				del word_list[w]
+				keep = np.random.random()
+				if keep >= original_accuracy:
+					j+=1
+					high_score_to_delete[w] = 1
+
+			for w in high_score_to_delete:
+				del high_scores[w]
+			if verbose:
+				print("Randomly removed", j, "words leaving us with", len(high_scores))
+
+		# add those high scoring words to the unambiguous list and retrain
+		self.remember(unambig_to_r = high_scores)
+	
+
 	def quick_tag(self, word, fc = True, max_guess = 3):
 		"""
 		quick and dirty tagger
@@ -202,6 +280,9 @@ class babyTagger:
 				tagged_entry = (1, word, (1, guess[1]))
 		
 		# UNKNOWN WORDS
+		elif re.search("[0-9]", word):
+			# if there's a numeral in it, just assume that it's a numeral
+			tagged_entry = (3, word, (1, "NUM"))	
 		else:
 			# we look at the top 'max_guess' best guesses
 			guess_matrix = [(0,0) for _ in range(max_guess)]
@@ -311,7 +392,7 @@ class mommaTagger:
 		if tagged_c_type == 1:
 			self.tag_trigrams, self.all_tags = self.read_tag_trigrams(tagged_c)
 		if tagged_c_type == 2:
-			self.tag_trigrams, self.all_tags = self.read_hand_tagged(tagged_c)
+			self.tag_trigrams, self.all_tags = self.read_hand_tagged(tagged_c, verbose)
 		self.mid_prob = self.build_mp_dict()
 		self.total_trigrams = self.count_all_trigrams()
 	
